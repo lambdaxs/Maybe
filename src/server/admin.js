038cb0015bd7b4033782979057307b28e05748b6
肖申 <xiaosheng@douyu.tv>
2018-09-30 16:57:40 +0800

    update

--- src/server/admin.js
+++ src/server/admin.js
@@ -112,7 +112,7 @@ router.post('/api/del',async(req,res)=>{
         colname:'apis',
     }).Model();
 
-    return model.delete({_id:ID(_id)}).then(rs=>{
+    return model.deleteOne({_id:ID(_id)}).then(rs=>{
         return res.json({code:0,data:rs})
     }).catch(err=>{
         return res.json({code:1,message:err.message})
